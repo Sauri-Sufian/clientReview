31683c96208915c1a302c42a87dbe26855c677af
saurisufian <abusauri.iub@gmail.com>
2021-03-23 04:18:44 +0600

    deploy_1

--- js/script.js
+++ js/script.js
@@ -8,40 +8,42 @@ const userImg= document.getElementById('user-image');
 const uniqueID=document.getElementById('info');
 const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
-
+const submitBtn = document.getElementById('submit-btn')
+const reviewScore=document.getElementById('score-review');
+const  icons = document.querySelectorAll('.fa-star');
 let userID=0;
 let userData=
 [
     {
     id:userID++,
-    name:"siam",
+    name:"Stanley Kubrick",
     imgUrl:`https://robohash.org/${userID}?set=set4`,
     score:"5",
-    text:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ratione et dolor. Esse aut quis quos eligendi perferendis voluptate quas sed quam quasi inventore magnam, magni possimus temporibus repellendus molestias.an"
+    text:" So, here we are. Why is Stanley Kubrick the best movie director of all time? Well, we don't have 3 hours to explain everything so here's a brief argument for this decision. If we can agree on the qualities that great directors must have, Kubrick simply checks all of the boxes."
 
     },
     {
     id:userID++,
-    name:"sufian",
+    name:"Alfred Hitchcock",
     imgUrl:`https://robohash.org/${userID}?set=set4`,
-    score:"5",
-    text:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ratione et dolor. Esse aut quis quos eligendi perferendis voluptate quas sed quam quasi inventore magnam, magni possimus temporibus repellendus molestias."
+    score:"4",
+    text:" There's a scene in Hitchcock, the biography starring Anthony Hopkins, where Alfred Hitchcock stands outside a packed theater as an audience watches Psycho for the first time. And when the infamous shower scene unfolds and the cacophony of screams erupts, Hitchcock emphatically waves his arms back and forth like an orchestral conductor."
 
     },
     {   
         id:userID++,
-        name:"bla",
+        name:"Akira Kurosawa",
         imgUrl:`https://robohash.org/${userID}?set=set4`,
-        score:"5",
-        text:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ratione et dolor. Esse aut quis quos eligendi perferendis voluptate quas sed quam quasi inventore magnam, magni possimus temporibus repellendus molestias.i"
+        score:"3",
+        text:" You don't have to understand Japanese culture or be a scholar of the country's Sengoku period to appreciate what Akira Kurosawa's work means. The point is Kurosawa took stories that are extremely specific and made them globally relevant. His stories took common themes from his own culture but also Shakespeare and found a message that resonated around the world."
     
     },
     {   
         id:userID++,
-        name:"jaha",
+        name:"Steven Spielberg",
         imgUrl:`https://robohash.org/${userID}?set=set4`,
-        score:"5",
-        text:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ratione et dolor. Esse aut quis quos eligendi perferendis voluptate quas sed quam quasi inventore magnam, magni possimus temporibus repellendus molestias."
+        score:"2",
+        text:"Steven Spielberg has been directing for over 50 years and shows no signs of stopping. Looking at his career, it's not just the sheer number of movies he's directed (30+), it's the amount of greatness on that list"
     
     }
 ]
@@ -60,8 +62,14 @@ const modalHandler=()=>{
     toggleBackDrop();
     showModal();
 }
+const resetIcon=()=>{
+    for (const iterator of icons) {
+        iterator.classList.remove('pos');
+    }
+}
 
 const showInitialContent=(user) =>{
+    resetIcon();
     initialCOntent.innerText=user.text;
     userName.innerText=user.name;
     uniqueID.innerText=user.id
@@ -69,8 +77,9 @@ const showInitialContent=(user) =>{
     `
     <img width="50px" src="${user.imgUrl}" alt="">
     ` 
-    
-    
+  for (let item=0;item<user.score;item++) {
+     icons[item].classList.add('pos')
+  }
 }
 
 const showPrevDataHandler=()=>{
@@ -94,11 +103,40 @@ const showNextDataHandler=()=>{
     showInitialContent(userData[index])
 
 }
+const clearData=(name,score,text)=>{
+    name.value="";
+    score.value="";
+    text.value="";
+}
 
+const newUserAddHandler=()=>{
+    const nameInput=document.getElementById('name-input');
+    const scoreInput=document.getElementById('score-input');
+    const textArea=document.getElementById('txt-area');
+
+    let newUser={
+        id:userID++,
+        name:nameInput.value,
+        imgUrl:`https://robohash.org/${userID}?set=set4`,
+        score:scoreInput.value,
+        text:textArea.value
+    }
+
+    
+    userData.push(newUser);
+    closeModalHandler();
+    clearData(nameInput,scoreInput,textArea);
+
+}
 
 
 backDrop.addEventListener('click',closeModalHandler)
 addReviewBtn.addEventListener('click',modalHandler);
 prevBtn.addEventListener('click',showPrevDataHandler);
 nextBtn.addEventListener('click',showNextDataHandler);
+submitBtn.addEventListener('click',newUserAddHandler);
+
+
+
+
 showInitialContent(userData[0]);
